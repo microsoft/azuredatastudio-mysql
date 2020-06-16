d90928c05826dcdd4b6902edeab12843f73b05c6
Kevin Mao <t-kemao@microsoft.com>
2020-06-16 19:28:54 -0400

    renaming pgtools to ostools

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-export const serviceName = 'pgSQLToolsService';
+export const serviceName = 'osToolsService';
 export const providerId = 'MySQL';
 export const serviceCrashMessage = 'PG SQL Tools Service component exited unexpectedly. Please restart Azure Data Studio.';
 export const serviceCrashButton = 'View Known Issues';
