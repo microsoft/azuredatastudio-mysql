dfaf679b24415905fdfab306b735571a87b8d0cd
rishky-msft <rishabhkumar@microsoft.com>
2023-01-20 17:41:54 +0530

    Moving bussiness logic to pgtoolsservice

--- src/utils/toolsserviceUtils.ts
+++ src/utils/toolsserviceUtils.ts
@@ -6,46 +6,34 @@
 import * as azdata from "azdata";
 import { SqlOpsDataClient } from "dataprotocol-client";
 import { SimpleExecuteRequest } from "dataprotocol-client/lib/protocol";
+import { CreateDatabaseRequest, GetCharsetsRequest, GetCollationsRequest } from "../contracts/contracts";
+import { CreateDatabaseRequestParams, GetCharsetsRequestParams, GetCharsetsResponse, GetCollationsRequestParams, GetCollationsResponse } from "../models/newDatabaseModels";
 
 export class ToolsServiceUtils {
 
-  private static CHARSET_QUERY = "SELECT CHARACTER_SET_NAME FROM INFORMATION_SCHEMA.CHARACTER_SETS"
-  private static COLLATION_QUERY_PREFIX = "SELECT COLLATION_NAME FROM INFORMATION_SCHEMA.COLLATIONS WHERE CHARACTER_SET_NAME="
-
   public static async getCharsets(ownerUri: string, client: SqlOpsDataClient): Promise<string[]> {
-    let result: azdata.SimpleExecuteResult = (await ToolsServiceUtils.runQuery(ownerUri, ToolsServiceUtils.CHARSET_QUERY, client));
-    var charset = result.rows.map((row) => {return row[0].displayValue;})
-    return charset;
+    let params: GetCharsetsRequestParams = {ownerUri: ownerUri};
+    let result: GetCharsetsResponse = (await client.sendRequest(GetCharsetsRequest.type, params));
+    return result.charsets;
   }
 
   public static async getCollations(ownerUri:string, charset: string | azdata.CategoryValue, client: SqlOpsDataClient): Promise<string[]> {
-    let result: azdata.SimpleExecuteResult = (await ToolsServiceUtils.runQuery(ownerUri, ToolsServiceUtils.getCollationQuery(charset), client));
-    var collation = result.rows.map((row) => {return row[0].displayValue;})
-    return collation;
+    let charsetvalue = (typeof charset === 'string') ? charset : charset.name;
+    let params: GetCollationsRequestParams = {ownerUri: ownerUri, charset: charsetvalue};
+    let result: GetCollationsResponse = (await client.sendRequest(GetCollationsRequest.type, params));
+    return result.collations;
   }
 
   public static async createDatabase(dbname: string, charset: string | azdata.CategoryValue, collation: string | azdata.CategoryValue, ownerUri: string, client: SqlOpsDataClient): Promise<void> {
-    await ToolsServiceUtils.runQuery(ownerUri, ToolsServiceUtils.getCreateDatabaseQuery(dbname, charset, collation), client);
+    let charsetvalue = (typeof charset === 'string') ? charset : charset.name;
+    let collationvalue = (typeof collation === 'string') ? collation : collation.name;
+    let params: CreateDatabaseRequestParams = {ownerUri: ownerUri, dbName: dbname, charset: charsetvalue, collation: collationvalue};
+    await client.sendRequest(CreateDatabaseRequest.type, params);
   }
 
   public static async runQuery(ownerUri: string, queryString: string, client: SqlOpsDataClient): Promise<azdata.SimpleExecuteResult> {
     let params: azdata.SimpleExecuteParams = { ownerUri: ownerUri, queryString: queryString }
     return await client.sendRequest(SimpleExecuteRequest.type, params);
-  }
-
-  private static getCreateDatabaseQuery(dbname: string, charset: string| azdata.CategoryValue, collation: string | azdata.CategoryValue): string {
-    let query = "CREATE DATABASE `" + dbname + "`";
-    if (charset) {
-      query += " CHARACTER SET='" + charset + "'";
-      if (collation) {
-        query += " COLLATE='" + collation + "'";
-      }
-    }
-    return query;
-  }
-
-  private static getCollationQuery(charset: string | azdata.CategoryValue): string {
-    return this.COLLATION_QUERY_PREFIX + "'" + charset + "'";
   }
 }
 
