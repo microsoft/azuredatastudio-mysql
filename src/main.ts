bebeaac3149a0842bb7941c45d8a208da3efad6e
rishky-msft <rishabhkumar@microsoft.com>
2023-02-10 16:32:25 +0530

    Revert "Firewall feature (#119)"
    This reverts commit d9bcf535b76c8e809625d19ceb453b29eb6c8497.

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,6 @@ import * as Utils from './utils';
 import { TelemetryReporter, LanguageClientErrorHandler } from './telemetry';
 import { TelemetryFeature } from './features/telemetry';
 import { registerDbDesignerCommands } from './features/dbDesigner';
-import { FireWallFeature } from './features/firewall'
 
 const baseConfig = require('./config.json');
 const outputChannel = vscode.window.createOutputChannel(Constants.serviceName);
@@ -55,9 +54,8 @@ export async function activate(context: vscode.ExtensionContext) {
 		features: [
 			// we only want to add new features
 			...SqlOpsDataClient.defaultFeatures,
-			TelemetryFeature,
-			FireWallFeature
-		],
+			TelemetryFeature
+		]
 	};
 
 	const installationStart = Date.now();
