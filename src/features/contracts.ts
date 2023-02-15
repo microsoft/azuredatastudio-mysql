c8747de02b3178fe6db48f9072fda6b739e19d19
rishky-msft <102506628+rishky-msft@users.noreply.github.com>
2023-02-15 10:16:00 +0530

    Revert "Firewall feature (#119)" (#126)
    This reverts commit d9bcf535b76c8e809625d19ceb453b29eb6c8497.

--- src/features/contracts.ts
+++ src/features/contracts.ts
@@ -3,12 +3,11 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as azdata from 'azdata';
-import { NotificationType, RequestType } from "vscode-languageclient";
+import { NotificationType } from "vscode-languageclient";
 import * as telemetry from '@microsoft/ads-extension-telemetry';
 
 
-// ------------------------------- < Telemetry Feature Events > ------------------------------------
+// ------------------------------- < Telemetry Sent Event > ------------------------------------
 
 /**
  * Event sent when the language service send a telemetry event
@@ -29,71 +28,5 @@ export class TelemetryParams {
         measures: telemetry.TelemetryEventMeasures;
     }
 }
-// ------------------------------- </ Telemetry Feature Events > ------------------------------------
+// ------------------------------- </ Telemetry Sent Event > ------------------------------------
 
-// ------------------------------- < Firewall Rule Feature Events > ------------------------------------
-
-/**
- * A request to open up a firewall rule
- */
-export namespace CreateFirewallRuleRequest {
-	export const type = new RequestType<CreateFirewallRuleParams, azdata.CreateFirewallRuleResponse, void, void>('resource/createFirewallRule');
-}
-
-/**
- * Firewall rule request handler
- */
-export namespace HandleFirewallRuleRequest {
-	export const type = new RequestType<HandleFirewallRuleParams, azdata.HandleFirewallRuleResponse, void, void>('resource/handleFirewallRule');
-}
-
-/**
- * Firewall rule creation parameters
- */
-export interface CreateFirewallRuleParams {
-	/**
-	 * Account information to use in connecting to Azure
-	 */
-	account: azdata.Account;
-	/**
-	 * Fully qualified name of the server to create a new firewall rule on
-	 */
-	serverName: string;
-	/**
-	 * Start of the IP address range
-	 */
-	startIpAddress: string;
-	/**
-	 * End of the IP address range
-	 */
-	endIpAddress: string;
-	/**
-	 * Firewall rule name
-	 */
-	firewallRuleName: string;
-	/**
-	 * Per-tenant token mappings. Ideally would be set independently of this call,
-	 * but for now this allows us to get the tokens necessary to find a server and open a firewall rule
-	 */
-	securityTokenMappings: {};
-}
-
-/**
- * Firewall rule handling parameters
- */
-export interface HandleFirewallRuleParams {
-	/**
-	 * The error code used to defined the error type
-	 */
-	errorCode: number;
-	/**
-	 * The error message from which to parse the IP address
-	 */
-	errorMessage: string;
-	/**
-	 * The connection type, for example MSSQL
-	 */
-	connectionTypeId: string;
-}
-
-// ------------------------------- </ Firewall Rule Feature Events > ------------------------------------
