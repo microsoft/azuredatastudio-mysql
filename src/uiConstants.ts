baadb1e256d879c38dd2d91274431fb63c81aed2
nitishsharma25 <112425736+nitishsharma25@users.noreply.github.com>
2023-02-22 12:14:39 +0530

    added localization to new db constants (#138)

--- src/uiConstants.ts
+++ src/uiConstants.ts
@@ -3,16 +3,18 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 'use strict';
+import * as nls from 'vscode-nls';
+const localize = nls.loadMessageBundle()
 
-export const NewDatabaseTitle = "New Database";
-export const NewDatabaseDialogName = "newDatabaseDialog"
-export const CreateButtonLabel = "Create"
-export const CancelButtonLabel = "Cancel"
-export const NewDatabaseDetailsTitle = "New Database Details"
-export const DatabaseNameTextBoxLabel = "Database Name TextBox"
-export const DatabaseNameTextBoxPlaceHolder = "Enter New Database Name"
-export const DatabaseNameLabel = "Name"
-export const DatabaseCharsetDropDownLabel = "Database Charset Dropdown"
-export const DatabaseCharsetLabel = "Charset"
-export const DatabaseCollationDropDownLabel = "Database Collation Dropdown"
-export const DatabaseCollationLabel = "Collation"
+export const NewDatabaseTitle = localize('newDatabaseTitle', 'New Database');
+export const NewDatabaseDialogName = localize('newDatabaseDialogName', 'newDatabaseDialog')
+export const CreateButtonLabel = localize('createButtonLabel', 'Create')
+export const CancelButtonLabel = localize('cancelButtonLabel', 'Cancel')
+export const NewDatabaseDetailsTitle = localize('newDatabaseDetailsTitle', 'New Database Details')
+export const DatabaseNameTextBoxLabel = localize('databaseNameTextBoxLabel', 'Database Name TextBox')
+export const DatabaseNameTextBoxPlaceHolder = localize('newDatabaseNameTextBoxPlaceHolder', 'Enter New Database Name')
+export const DatabaseNameLabel = localize('databaseNameLabel', 'Name')
+export const DatabaseCharsetDropDownLabel = localize('databaseCharsetDropDownLabel', 'Database Charset Dropdown')
+export const DatabaseCharsetLabel = localize('databaseCharsetLabel', 'Charset')
+export const DatabaseCollationDropDownLabel = localize('databaseCollationDropDownLabel', 'Database Collation Dropdown')
+export const DatabaseCollationLabel = localize('databaseCollationLabel', 'Collation')
